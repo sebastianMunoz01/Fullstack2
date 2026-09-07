0fbec0626ce378ccce049deb4bc23b08ff636141
seba <sebastian.apart@gmail.com>
2026-09-07 00:35:54 -0300

    HTML catalogo parcialmente realizado con contenedor y menu desplegable

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
 function inyectarFooter(){
-    document.getElementById("footer").innerHTML = "<p>mafiosos unidos spa</p>";
+    document.getElementById("footer").innerHTML = "<p>Pawchi 2026.</p>";
 }
 
 function inyectarHeader(){
-    document.getElementById("header").innerHTML = '<section><h1 class="prevent-select">Pawchi</h1></section><section><a href="#inicio">inicio</a><a href="#informacion">informacion</a><a href="#contacto">contacto</a></section><section><p class="prevent-select">pone tu sueldo, lo vas duplicar <span>🤑</span></p></section>';
+    document.getElementById("header").innerHTML = '<nav class="nav-menu"><div class="dropdown"><button class="dropbtn">Menu</button><div class="dropdown-content"><a href="index.html">Inicio</a><a href="catalogo.html">Catalogo</a><a href="nosotros.html">Nosotros</a></div></div></nav><section><h1 class="prevent-select">Pawchi</h1></section><section><p class="prevent-select">Que pawchi te tocara?</span></p></section>';
 }
 
 inyectarHeader();
